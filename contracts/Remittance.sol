519bf0786ac84b5c62bd5039720c42f11e7dfff8
Christopher Milner <chris.milner@gmail.com>
2018-10-24 01:56:41 +0700

    Redesign after comments from tutors, yet to be tested for compilation in Remix

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -2,19 +2,16 @@ pragma solidity ^0.4.24;
 
 contract Remittance {
 
-struct Transaction {
+struct Box {
   uint amount;
-  address sender;
-  address receiver;
   bytes32 passwordHash;
 }
 
-mapping(address => uint) balances;
-mapping(uint => Transaction) transactions;
+mapping(uint => Box) boxes;
 
 address owner;
 
-uint transactionNumber;
+uint boxNumber;
 
 event LogSendFunds(address, uint);
 event LogPasswordError(string);
@@ -29,68 +26,35 @@ modifier isOwner() {
   _;
 }
 
-function fundAccount(address _receiver) public payable {
+function createBox() public payable {
   require(msg.value > 0);
   require(msg.sender != address(0));
-
-  balances[msg.sender] += msg.value;
-  sendPassword(msg.sender, _receiver, msg.value);
-
-}
-
-function getBalance(address account) public returns (uint) {
-  return balances[account];
-}
-
-function sendFunds(address sender, address receiver, uint amount) internal {
-  require(receiver !=0);
-  require(amount > 0);
-  require(balances[sender] >= amount);
-
-  receiver.transfer(amount);
-  balances[receiver] -= amount;
-  emit LogSendFunds(receiver, amount);
-}
-
-function sendPassword(address _sender, address _receiver, uint _amount) private returns (string, string) {
-  string memory passwordSender = "test123";
-  string memory passwordReceiver = "test456";
-
   bytes32 pwHash;
-  transactionNumber += 1;
+  string password1 = "test123";
+  string password2 = "test456";
 
-  pwHash = keccak256(passwordSender, passwordReceiver);
-  transactions[transactionNumber].amount = _amount;
-  transactions[transactionNumber].receiver = _receiver;
-  transactions[transactionNumber].sender = _sender;
-  transactions[transactionNumber].passwordHash = pwHash;
+  boxNumber += 1;
+  boxes[boxNumber].amount = msg.value;
 
+  pwHash = keccak256(password1, password2);
+  boxes[boxNumber].passwordHash = pwHash;
   emit LogPasswordGenerated(passwordSender, passwordReceiver);
-
-  return (passwordSender, passwordReceiver);
 }
 
-function checkPasswords(uint transNo, string password1, string password2) public payable {
+function openBox(uint boxNo, string password1, string password2) public payable {
    bytes32 passwordHash;
-   uint amountToSend;
-   address sender;
-   address receiver;
-   receiver = msg.sender;
-   require (receiver == transactions[transNo].receiver);
-   sender = transactions[transNo].sender;
+   uint amountToRelease;
+
    passwordHash = keccak256(password1, password2);
-   if (transactions[transNo].passwordHash == passwordHash) {
-     amountToSend = transactions[transNo].amount;
-     sendFunds(sender, receiver, amountToSend);
+
+   if (boxes[boxNo].passwordHash == passwordHash) {
+     amountToRelease = boxes[boxNo].amount;
+     msg.sender.transfer(amount);
+     boxes[boxNo].amount = 0;
    } else {
        LogPasswordError("Password mismatch");
    }
 
 }
-
-function getTransactionHash(uint transNo) public view returns (bytes32) {
-    return transactions[transNo].passwordHash;
-}
-
 
 }
